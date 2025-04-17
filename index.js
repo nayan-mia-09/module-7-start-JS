438a7691d059f30efecc2ae5feccdb3c7c91ef7e
Nayan <dreamerboynayan@gmail.com>
2025-04-17 06:15:36 +0600

    Primitive Data Type

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // JS Variable 
 
-var a = 10;
-let b = 20;
-const PI = 3.1416;
+// var a = 10;
+// let b = 20;
+// const PI = 3.1416;
 
-console.log(a, b, PI);
+// console.log(a, b, PI);
+
+// Primitive Data Type 
+
+let name = "Ranvit Kapoor";
+let age = 47;
+let isActor = true;
+let x;
+let y = null;
+
+console.log(name); // Ranvir Kapoor
+console.log(age); // 47
+console.log(isActor); // true
+console.log(x); // undefined
+console.log(y); // null
