cd1750a18450d9a75a1b29b5828976222786084e
Nayan <dreamerboynayan@gmail.com>
2025-04-17 06:21:59 +0600

    Non Primitive Data Type

--- index.js
+++ index.js
@@ -8,14 +8,29 @@
 
 // Primitive Data Type 
 
-let name = "Ranvit Kapoor";
-let age = 47;
-let isActor = true;
-let x;
-let y = null;
+// let name = "Ranvit Kapoor";
+// let age = 47;
+// let isActor = true;
+// let x;
+// let y = null;
 
-console.log(name); // Ranvir Kapoor
-console.log(age); // 47
-console.log(isActor); // true
-console.log(x); // undefined
-console.log(y); // null
+// console.log(name); // Ranvir Kapoor
+// console.log(age); // 47
+// console.log(isActor); // true
+// console.log(x); // undefined
+// console.log(y); // null
+
+// Non Primitive Data Type
+
+let person = {
+    name: "Kareem Amzad",
+    age : 49,
+    city: "Dhaka"
+};
+
+console.log(person); // {name:"Kareem Amzad", age: 49 , city:"Dhaka"}
+console.log(typeof person); // object
+
+let colors =["red", "yellow", "blue", "green"];
+console.log(colors); //["red", "yellow", "blue", "green"];
+console.log(typeof colors);// object
